f1351531a6f529415343d3f77990946e3438988b
wucan <wucan@geo-compass.com>
2018-01-20 18:18:54 +0800

    恢复config的URL

--- static/config.js
+++ static/config.js
@@ -1,4 +1,4 @@
-const SERVER_URL = "http://127.0.0.1:7002";
+const SERVER_URL = "http://127.0.0.1:7001";
 // const SERVER_URL = 'http://47.93.225.139:7001'
 const CONFIG = {
   MANAGE_URL: "http://47.93.225.139:8080/manage/",
